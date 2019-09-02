0f11659026e65087c43258a07344f4deef74b230
shota <ppddccaa165@gmail.com>
2019-09-02 18:11:28 +0900

    [finish]rails-lesson

---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,9 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+Refile.secret_key = '18b90eb20bd28c0b9ed6050c23b169c9cbad9f25c203dd9ed85121f067182552ab376ec4cbb76929f352f2496457dfc955363f1ac1669c8ddc61be4b3a99743e'
